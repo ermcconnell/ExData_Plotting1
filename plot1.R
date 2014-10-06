93b6722d7ff8cdb5223c0892c027e695fe9dbf88
Emma <mcconnell.emma@gmail.com>
2014-10-06 15:37:46 -0700

    Project1-InitialResults
    These are my results for all four plots after my first attempt. Needs to
    be checked again for accuracy before submitting. Needs comments.

---
+++ plot1.R
@@ -0,0 +1,11 @@
+temp<-tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data<-read.table(unz(temp,"household_power_consumption.txt"), sep=";", header=TRUE, na.strings="?")
+unlink(temp)
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+data$Time<-strptime(data$Time,"%H:%M:%S")
+dataSm <-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
+
+png(filename="C:/Users/EMcConnell/Documents/GitHub/ExData_Plotting1/plot1.png". width=480, height=480)
+hist(dataSm$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,18 @@
+temp<-tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data<-read.table(unz(temp,"household_power_consumption.txt"), sep=";", header=TRUE, na.strings="?")
+unlink(temp)
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+data$Time2<-strptime(data$Time,"%H:%M:%S")
+dataSm <-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
+
+dataSm$datetime<-as.POSIXct(paste(dataSm$Date, dataSm$Time), format = "%Y-%m-%d %H:%M:%S")
+
+png(filename="C:/Users/EMcConnell/Documents/GitHub/ExData_Plotting1/plot3.png", width=480, height=480)
+plot(dataSm$Sub_metering_1~dataSm$datetime, type="l",ylab="Energy sub metering",xlab=NA)
+lines(dataSm$datetime,dataSm$Sub_metering_2,col="red")
+lines(dataSm$datetime,dataSm$Sub_metering_3,col="blue")
+Leg<-c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+legend(x="topright",legend=Leg, col=c("black","red","blue"), lty=c(1,1,1))
+dev.off()
+
