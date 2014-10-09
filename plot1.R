554b812b1d5b328607b5a6f13ff50216ec54647e
Emma <mcconnell.emma@gmail.com>
2014-10-09 10:33:51 -0700

    Update1
    Commented my code

--- plot1.R
+++ plot1.R
@@ -1,11 +1,15 @@
+#Download Data
 temp<-tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
 data<-read.table(unz(temp,"household_power_consumption.txt"), sep=";", header=TRUE, na.strings="?")
 unlink(temp)
+
+#Format date and time, exclude uncessary dates
 data$Date<-as.Date(data$Date,format="%d/%m/%Y")
 data$Time<-strptime(data$Time,"%H:%M:%S")
-dataSm <-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
+dataSm<-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
 
+#Initiate the PNG file I want to plot to
 png(filename="C:/Users/EMcConnell/Documents/GitHub/ExData_Plotting1/plot1.png". width=480, height=480)
 hist(dataSm$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,12 +1,16 @@
+#Download data
 temp<-tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
 data<-read.table(unz(temp,"household_power_consumption.txt"), sep=";", header=TRUE, na.strings="?")
 unlink(temp)
+
+#Format date and time
 data$Date<-as.Date(data$Date,format="%d/%m/%Y")
 data$Time2<-strptime(data$Time,"%H:%M:%S")
 dataSm <-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
-
 dataSm$datetime<-as.POSIXct(paste(dataSm$Date, dataSm$Time), format = "%Y-%m-%d %H:%M:%S")
+
+#Initiate PNG file to save plot to
 png(filename="C:/Users/EMcConnell/Documents/GitHub/ExData_Plotting1/plot2.png", width=480, height=480)
 plot(dataSm$Global_active_power~dataSm$datetime, type="l",ylab="Global Active Power (kilowatts)",xlab=NA)
 dev.off()
